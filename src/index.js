3e76598b31430fb98b01f9067470b6ad7ee9381b
Kratharth <kratharthhegde.cs17@bmsce.ac.in>
2019-06-23 19:25:58 +0530

    Modified index.js file

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Login from './Login';
-import Homepage from './home-page';
-import Exam from './exam';
 import * as serviceWorker from './serviceWorker';
 import App from './App';
-ReactDOM.render(<Login/>, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
